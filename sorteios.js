e139ebccae834fd2e1b707e8398a30eb4c4f1502
cheddarmcmelt <gadc3110@gmail.com>
2026-04-24 10:01:42 -0300

    Atualização

---
+++ sorteios.js
@@ -0,0 +1,49 @@
+ganhoEL = document.getElementById("ganho");
+
+let ValorGanho = 0
+
+function rara(carta, valorDaAposta){
+    carta.style.backgroundImage = 'url(Scr/Assets/Cards/Rara.png)';
+       
+        ValorGanho = ValorGanho + (3 * valorDaAposta);
+        console.log(ValorGanho);
+        ganhoEL.innerHTML = `Triplicou! Ganhou ${ValorGanho}`;
+}
+function incomum(carta, valorDaAposta){
+    carta.style.backgroundImage = "url(Scr/Assets/Cards/Incomum.png)";
+
+    ValorGanho = ValorGanho + (2 * valorDaAposta)
+    console.log(ValorGanho)
+    ganhoEL.innerHTML = `Duplicou! Ganhou ${ValorGanho}`; 
+}
+function comum(carta, valorDaAposta){ 
+    carta.style.backgroundImage = "url(Scr/Assets/Cards/Comum.png)";
+
+    ValorGanho = ValorGanho + valorDaAposta
+    console.log(ValorGanho)
+    ganhoEL.innerHTML = `Se pagou! Recebeu ${ValorGanho}`;
+}
+
+function bomba(carta){
+    const modal = document.getElementById("modal")
+
+    carta.style.backgroundImage = "url(Scr/Assets/Cards/Bomba.png)";
+    ValorGanho = 0
+    console.log(ValorGanho)
+    ganhoEL.innerHTML = `Perdeu tudo!`;
+    modal.showModal()
+}
+
+function duplicador(carta){
+    carta.style.backgroundImage = "url(Scr/Assets/Cards/Dublicador.png)";
+    ValorGanho = ValorGanho * 2
+    console.log(ValorGanho)
+    ganhoEL.innerHTML = `Dobrou! Ganhou ${ValorGanho}`;
+}
+
+function divididor(carta){
+    carta.style.backgroundImage = "url(Scr/Assets/Cards/Faca.png)";
+    ValorGanho = ValorGanho / 2
+    console.log(ValorGanho)
+    ganhoEL.innerHTML = `Pela metade! Restou ${ValorGanho}`;
+}
---
+++ timer.js
@@ -0,0 +1,18 @@
+*{
+    font-family: comic sans ms;
+    user-select: none;
+    background-color: blue;
+}
+section{
+    display: flex;
+}
+
+.card {
+    margin: 10px;
+    width: 150px;
+    height: 200px;
+    background-image: url(Assets/Cards/Tras.png);
+}
+p, label, input, button {
+    color: white;
+}
