03d56522e6de6637955a6b4e41c1713fc6fc75b5
Lifante <lifantematheus@gmail.com>
2026-05-06 19:12:11 -0300

    Atualizando + Adicionando verificação de saldo
    Implement user registration functionality with input validation.

---
+++ Scr/cadastro.js
@@ -0,0 +1,43 @@
+const telaCadastro = document.getElementById("Cadastro");
+
+const btnCad = document.getElementById("btnCad");
+
+const impNome = document.getElementById("nome");
+const impSaldo = document.getElementById("saldo");
+
+impNome.style.borderColor = "white";
+impSaldo.style.borderColor = "white";
+
+function cadastrar () {
+  console.log(  'sim')
+  let nome = document.getElementById("nome").value;
+  let saldo = parseInt(document.getElementById("saldo").value);
+  
+
+  if ((nome === "") || ((isNaN(saldo) === true || (saldo < 10)))) {
+    impNome.style.borderColor = "white";
+    impSaldo.style.borderColor = "white";
+
+    if (nome === ""){
+      impNome.style.borderColor = "red";
+      alert("Preencha o nome.");
+    }
+    if ((isNaN(saldo) === true)){
+      impSaldo.style.borderColor = "red";
+      alert("Preencha o saldo atual corretamente.");
+    }
+    if (saldo < 10){
+      impSaldo.style.borderColor = "red";
+      alert("O saldo atual deve ser maior que 10.");
+    }
+
+  } else {
+    console.log (saldo)
+    localStorage.setItem("saldo", saldo)
+
+
+    console.log(isNaN(saldo));
+    window.location.href = "jogo.html";
+
+  }
+};
